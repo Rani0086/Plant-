a1c49c7a4e15684c431b3dc272e0d981e814794a
Rani0086 <shantidhaakad@gmail.com>
2024-11-13 21:11:37 +0530

    Initial commit

---
+++ config.py
@@ -0,0 +1,16 @@
+from os import getenv
+
+from dotenv import load_dotenv
+
+load_dotenv()
+
+API_ID = "6435225"
+# -------------------------------------------------------------
+API_HASH = "4e984ea35f854762dcde906dce426c2d"
+# --------------------------------------------------------------
+BOT_TOKEN = getenv("BOT_TOKEN", None)
+MONGO_URL = getenv("MONGO_URL", None)
+OWNER_ID = int(getenv("OWNER_ID", "7526369190"))
+SUPPORT_GRP = "waifexanime"
+UPDATE_CHNL = "Planetsadala"
+OWNER_USERNAME = "queen143np"
