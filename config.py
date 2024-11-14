de621b79fc029d6d97f5f9df4d8ed4bd9116b9b1
Goku malik <168086405+Gokukhan1@users.noreply.github.com>
2024-11-14 10:03:38 +0530

    Update config.py

--- config.py
+++ config.py
@@ -14,25 +14,3 @@ OWNER_ID = int(getenv("OWNER_ID", "7526369190"))
 SUPPORT_GRP = "waifexanime"
 UPDATE_CHNL = "Planetsadala"
 OWNER_USERNAME = "queen143np"
-
-# Tokens
-
-Riz = TelegramClient('Riz', API_ID, API_HASH).start(bot_token=BOT_TOKEN)
-
-Riz2 = TelegramClient('Riz2', API_ID, API_HASH).start(bot_token=BOT_TOKEN2)
-
-Riz3 = TelegramClient('Riz3', API_ID, API_HASH).start(bot_token=BOT_TOKEN3)
-
-Riz4 = TelegramClient('Riz4', API_ID, API_HASH).start(bot_token=BOT_TOKEN4)
-
-Riz5 = TelegramClient('Riz5', API_ID, API_HASH).start(bot_token=BOT_TOKEN5)
-
-Riz6 = TelegramClient('Riz6', API_ID, API_HASH).start(bot_token=BOT_TOKEN6)
-
-Riz7 = TelegramClient('Riz7', API_ID, API_HASH).start(bot_token=BOT_TOKEN7)
-
-Riz8 = TelegramClient('Riz8', API_ID, API_HASH).start(bot_token=BOT_TOKEN8)
-
-Riz9 = TelegramClient('Riz9', API_ID, API_HASH).start(bot_token=BOT_TOKEN9)
-
-Riz10 = TelegramClient('Riz10', API_ID, API_HASH).start(bot_token=BOT_TOKEN10)
